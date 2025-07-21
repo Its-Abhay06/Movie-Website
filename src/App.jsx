c957c50eb5b6f1d67e6ef7cbb5ddc0b0239d02c0
Abhay Tripathi <abhay.mani29@gmail.com>
2025-07-21 14:05:19 +0530

    started with search component and initial front look

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Search from './Components/Search'
+import { useState } from 'react'
+
+function App() {
+  const[searchTerm,setSearchTerm]=useState('');
+  return (
+    <main>
+      <div className="pattern"/>
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="hero banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      </div>
+    </main>
+  )
+}
+
+export default App
